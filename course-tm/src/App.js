2c59571a27f416574ea073dae8d9f16d64a668d7
frey <freyprogrammer@gmail.com>
2020-01-07 00:40:06 +0800

    create postform

--- course-tm/src/App.js
+++ course-tm/src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 
 import Posts from './components/Posts';
-// import Postform from './components/Postform';
+import Postform from './components/Postform';
 
 function App() {
   return (
     <div className="App">
-      {/* <Postform /> */}
+      <Postform />
       <Posts />
     </div>
   );
